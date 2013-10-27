5c307503cfeddecca86039bde123e7c82f370a0e
Grant Byrne <Grant@GrantByrne.com>
2013-10-27 13:13:53 -0400

    Initial Commit
    Just a basic Yeoman scaffolding.

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('WowThatWasHelpfulApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
